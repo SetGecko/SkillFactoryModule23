c2b043b6d855f86f5725041f29e869eca9d67d6c
Ипполитов Алексей <81488631+SetGecko@users.noreply.github.com>
2023-07-11 11:01:14 +0300

    Create Task_23.3.2.sql

---
+++ Task_23.3.2.sql
@@ -0,0 +1,18 @@
+CREATE VIEW orders_for_kazan AS 
+SELECT 
+  name, 
+  amount, 
+  order_id 
+FROM 
+  products 
+  JOIN positions ON products.id = positions.product_id 
+WHERE 
+  positions.order_id IN (
+    SELECT 
+      id 
+    FROM 
+      orders 
+    WHERE 
+      address = 'Казань' 
+      AND status = 'in progress'
+  );
