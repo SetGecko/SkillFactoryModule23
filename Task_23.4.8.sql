377198cd61774817e1cd91136c1a0c236c0d20e8
Ипполитов Алексей <81488631+SetGecko@users.noreply.github.com>
2023-07-11 20:47:12 +0300

    Update Task_23.4.8.sql

--- Task_23.4.8.sql
+++ Task_23.4.8.sql
@@ -1,1 +1,10 @@
-
+ALTER TABLE orders ADD COLUMN date_changed TIMESTAMP;
+CREATE OR REPLACE FUNCTION update_date_changed() RETURNS TRIGGER AS $$
+BEGIN
+   NEW.date_changed = now();
+   RETURN NEW;  
+END;
+$$ language 'plpgsql';
+CREATE TRIGGER update_date_changed_triger
+BEFORE UPDATE ON orders
+FOR EACH ROW EXECUTE PROCEDURE update_date_changed();
