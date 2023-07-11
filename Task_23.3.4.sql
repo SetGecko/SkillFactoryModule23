4e8474862bdacc9c198d72d30d9322de53605cdf
Ипполитов Алексей <81488631+SetGecko@users.noreply.github.com>
2023-07-11 11:04:02 +0300

    Create Task_23.3.4.sql

---
+++ Task_23.3.4.sql
@@ -0,0 +1,11 @@
+SELECT 
+  orders.id, 
+  SUM(
+    positions.amount * products.price
+  ) 
+FROM 
+  orders 
+  JOIN positions ON orders.id = positions.order_id 
+  JOIN products ON positions.product_id = products.id 
+GROUP BY 
+  orders.id;
